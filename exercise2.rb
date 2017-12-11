b584869e6d175b8f90c2eec4a745bf70353c28ac
Scott <scotthuston1982@gmail.com>
2017-12-11 13:27:42 -0500

    exercise 3 has begun

---
+++ exercise2.rb
@@ -0,0 +1,8 @@
+tip = 55 * 0.2
+puts "the tip is #{tip} dollars"
+
+puts "5 " + "people are present"
+
+puts "45628 multiplied by 7839 is equel to #{45628 * 7839}"
+
+(10 < 20 && 30 < 20) || !(10 == 11)
